524dba7cff73596b395c5643dfd71f5ddac5f5b8
Derik-Carv <derikcarv.dc@gmail.com>
2024-10-15 16:44:53 -0300

    validando cnpj

--- src/index.ts
+++ src/index.ts
@@ -37,19 +37,45 @@ class ValidatorSea {
     }
     
     public static validoCPF(cpf: string): boolean {
-        const cleaned = cpf.replace(/\D/g, '');
-        if (cleaned.length !== 11) return false;
-        const sum1 = cleaned.split('').slice(0, 9).reduce((acc, digit, index) => acc + parseInt(digit) * (10 - index), 0);
+        const clean = cpf.replace(/\D/g, '');
+        if (clean.length !== 11) return false;
+        const sum1 = clean.split('').slice(0, 9).reduce((acc, digit, index) => acc + parseInt(digit) * (10 - index), 0);
         const firstDigit = (sum1 * 10) % 11 === 10 ? 0 : (sum1 * 10) % 11;
-        const sum2 = cleaned.split('').slice(0, 10).reduce((acc, digit, index) => acc + parseInt(digit) * (11 - index), 0);
+        const sum2 = clean.split('').slice(0, 10).reduce((acc, digit, index) => acc + parseInt(digit) * (11 - index), 0);
         const secondDigit = (sum2 * 10) % 11 === 10 ? 0 : (sum2 * 10) % 11;
-        return cleaned[9] === String(firstDigit) && cleaned[10] === String(secondDigit);
+        return clean[9] === String(firstDigit) && clean[10] === String(secondDigit);
+    }
+    public static mascaraCNPJ(cnpj: string): string {
+        return cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d)/, '$1.$2.$3/$4-$5');
     }
     
+    public static validoCNPJ(cnpj: string): boolean {
+        const clean = cnpj.replace(/\D/g, '');
+        if (clean.length !== 14) return false;
+        const firstDigits = clean.slice(0, 12);
+        let sum1 = 0, weight = 5;
+        for (let i = 0; i < firstDigits.length; i++) {
+            sum1 += parseInt(firstDigits[i]) * weight;
+            weight = weight === 2 ? 9 : weight - 1;
+        }
+        const firstDigit = this.digitoValido(sum1);
+    
+        const secondDigits = clean.slice(0, 13);
+        let sum2 = 0;
+        weight = 6;
+        for (let i = 0; i < secondDigits.length; i++) {
+            sum2 += parseInt(secondDigits[i]) * weight;
+            weight = weight === 2 ? 9 : weight - 1;
+        }
+        const secondDigit = this.digitoValido(sum2);
+    
+        return clean[12] === String(firstDigit) && clean[13] === String(secondDigit);
+    }
 }
     const numberCel = "21912345678";
     const cpf = "123.456.789-09";
+    const cnpj = "12.345.678/0001-95";
     console.log("Celular: ", ValidatorSea.validoCel(numberCel));
     console.log("Celular:", ValidatorSea.mascaraCel(numberCel), "Válido?", ValidatorSea.validoCel(numberCel));
     console.log("CPF:", ValidatorSea.mascaraCPF(cpf), "Válido?", ValidatorSea.validoCPF(cpf));
-    
+    console.log("CNPJ:", ValidatorSea.mascaraCNPJ(cnpj), "Válido?", ValidatorSea.validoCNPJ(cnpj));
